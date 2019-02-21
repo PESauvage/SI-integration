202918150444a604e6c0483718085de8283d2099
Paul-Emile Sauvage <sauvage.paulemile@gmail.com>
2019-02-21 11:24:21 +0100

    Changed background color, bug fixing and resize of different div

--- scripts/ecosystem.js
+++ scripts/ecosystem.js
@@ -1,8 +1,29 @@
 let sr=ScrollReveal()
 
+sr.reveal('.accroche',{
+    duration:700,
+    delay: 300,
+    distance:'300px',
+    reset:false,
+})
+
 sr.reveal('.AM',{
+    duration:500,
+    delay: 600,
+    distance:'500px',
+    reset:false,
+})
+
+sr.reveal('.AirPlay',{
     duration:500,
     delay: 300,
     distance:'300px',
-    reset:true,
+    reset:false,
+})
+
+sr.reveal('.Homekit',{
+    duration:500,
+    delay: 300,
+    distance:'300px',
+    reset:false,
 })
